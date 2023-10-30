519a57bd4174a89f8bf1c0ffb3646e71ce505a02
rahul <rdhone366@gmail.com>
2023-10-30 21:57:48 +0530

    my first commit

---
+++ wranglerhomepage/index.js
@@ -0,0 +1,72 @@
+// function showTag() {
+//     let hidden = document.getElementById('hidden');
+//     hidden.style.display = 'block';
+   
+// }
+
+// function hideTag() {
+//     let hidden = document.getElementById('hidden');
+//     hidden.style.display = 'none';
+  
+// }
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function showTag1() {
+//     let hidden = document.getElementById('hidden1');
+//     hidden.style.display = 'block';
+// }
+
+// function hideTag1() {
+//     let hidden = document.getElementById('hidden1');
+//     hidden.style.display = 'none';
+// }
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function showTag2() {
+//     let hidden = document.getElementById('hidden2');
+//     hidden.style.display = 'block';
+// }
+
+// function hideTag2() {
+//     let hidden = document.getElementById('hidden2');
+//     hidden.style.display = 'none';
+// }
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function showTag3() {
+//     let hidden = document.getElementById('hidden3');
+//     hidden.style.display = 'block';
+// }
+
+// function hideTag3() {
+//     let hidden = document.getElementById('hidden3');
+//     hidden.style.display = 'none';
+// }
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function showTag4() {
+//     let hidden = document.getElementById('hidden4');
+//     hidden.style.display = 'block';
+// }
+
+// function hideTag4() {
+//     let hidden = document.getElementById('hidden4');
+//     hidden.style.display = 'none';
+// }
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function showTag5() {
+//     let hidden = document.getElementById('hidden5');
+//     hidden.style.display = 'block';
+// }
+
+// function hideTag5() {
+//     let hidden = document.getElementById('hidden5');
+//     hidden.style.display = 'none';
+// }
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function showTag6() {
+//     let hidden = document.getElementById('hidden6');
+//     hidden.style.display = 'block';
+// }
+
+// function hideTag6() {
+//     let hidden = document.getElementById('hidden6');
+//     hidden.style.display = 'none';
+// }
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
